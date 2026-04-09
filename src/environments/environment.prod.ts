2322bad224f513c6b1725c7470aacff696801fd8
Dingwall <arding@umich.edu>
2026-04-09 17:07:54 -0500

    rebuild and deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  supabaseUrl: 'https://gldnrofhmyqjiqrhygzp.supabase.co',
+  supabaseKey: 'sb_publishable_5GxVJ4WbB2lMhrhtTKIeLQ_mZk685Q9'
 };
